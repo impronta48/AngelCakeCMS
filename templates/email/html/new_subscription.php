d3ecf37eb00f6314277863c643d218b87bee8ef0
Massimo Infunti <massimoi@nkoni.org>
2020-10-07 12:31:29 +0200

    correzione subscription

---
+++ templates/email/html/new_subscription.php
@@ -0,0 +1,6 @@
+Gentile <?= $user->first_name; ?>,<br>
+abbiamo ricevuto la tua richiesta di abbonamento.<br>
+<br>
+La tua richiesta è stata presa in carico. Verrai avvisato appena l'abbonamento è pronto.<br>
+Saluti,<br>
+Lo Staff di iMpronta<br>
