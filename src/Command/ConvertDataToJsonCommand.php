bb3ae0da69fe9c250da6d562663b232b3d9f0a2f
Alessandro <alessandro@bikesquare.eu>
2021-03-31 18:10:49 +0200

    Converti anche localizzazioni del campo data

--- src/Command/ConvertDataToJsonCommand.php
+++ src/Command/ConvertDataToJsonCommand.php
@@ -18,6 +18,7 @@ class ConvertDataToJsonCommand extends Command
 	public function execute(Arguments $args, ConsoleIo $io) {
 		$poitable = TableRegistry::getTableLocator()->get('Poi');
 		$pois = $poitable->find();
+		$issues = 0;
 		$io->out('Caricati Pois\nConversione campo data...');
 
 		foreach ($pois as $poi) {
@@ -31,14 +32,55 @@ class ConvertDataToJsonCommand extends Command
 				if (!$data) { // Still could not unserialize! Weird
 					$io->out("[!] Impossibile deserializzare dati del POI #{$poi->id}");
 					$io->out($poi->dataold);
+					$issues++;
 					continue;
 				}
   				$data = array_map('utf8_encode', $data ); // Encode data again
 			}
-
 			$poi->data = json_encode($data);
+			$io->out($poi->data);
 			$poitable->save($poi);
 		}
-		$io->out('Finito');
+		$io->out("Finito con $issues problemi");
+
+		$io->out('\nConversione traduzioni campo data...');
+		$i18ntable = TableRegistry::getTableLocator()->get('I18n');
+		$localizations = $i18ntable->find()->where(['model' => 'Poi', 'field' => 'data']);
+		$issues = 0;
+		foreach ($localizations as $loc) {
+			if (empty($loc->content) || $loc->content == 'a:0:{}') {
+				$loc->content = null;
+				$loc->setDirty('content');
+				$i18ntable->save($loc);
+				continue;
+			}
+
+			$data = @unserialize($loc->content);
+			if (!$data) {
+  				$data =  @unserialize(utf8_decode($loc->content)); // Decode first
+				if (!$data) { // Try to fix field declared length
+					$data = @unserialize(preg_replace_callback(
+        				'!s:(\d+):"(.*?)";!s',
+        				function($m){
+        				    $len = strlen($m[2]);
+        				    return "s:$len:\"{$m[2]}\";";
+        				},
+        				$data));
+					if (!$data) {
+						$io->out("[!] Impossibile deserializzare traduzione del POI #{$loc->foreign_key}");
+						$io->out($loc->content);
+						$issues++;
+						continue;
+					}
+				} else { // Encode data again
+  					$data = array_map('utf8_encode', $data );
+				}
+			}
+
+			$loc->content = json_encode($data);
+			$io->out($loc->content);
+			$i18ntable->save($loc);
+		}
+		$io->out("Finito con $issues problemi");
 	}
 }
