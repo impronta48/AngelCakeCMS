5997fd054ffcb744d1aba8e0b681295f03122f66
Massimo Infunti <massimoi@nkoni.org>
2021-04-01 20:09:21 +0200

    destinations filtrate

--- src/Controller/DestinationsController.php
+++ src/Controller/DestinationsController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -16,10 +17,10 @@ use Psr\Log\LogLevel;
 class DestinationsController extends AppController
 {
 
-	public $paginate = [
-	'limit' => 12,
-	];
-	
+  public $paginate = [
+    'limit' => 50,
+  ];
+
   /**
    * Index method
    *
@@ -27,41 +28,43 @@ class DestinationsController extends AppController
    */
   public function index()
   {
-        $query = $this->Destinations->find()
-            ->contain(['Articles'])
-            ->select(['id', 'name', 'slug', 'nazione_id', 'regione', 'nomiseo', 'published', 'show_in_list', 'created', 'modified']);
+    $query = $this->Destinations->find()
+      ->contain(['Articles'])
+      ->select(['id', 'name', 'slug', 'nazione_id', 'regione', 'nomiseo', 'published', 'show_in_list', 'created', 'modified'])
+      ->order(['nazione_id', 'name'])
+      ->where(['published' => true]);
 
-        $random = $this->request->getQuery('random');
-        if (!empty($random)) {
-            $query->order('rand()');
-        }
+    $random = $this->request->getQuery('random');
+    if (!empty($random)) {
+      $query->order('rand()');
+    }
 
-        $only = $this->request->getQuery('only');
-        if (!empty($only)) {
-            $query->select(explode(',', $only));
-        }
+    $only = $this->request->getQuery('only');
+    if (!empty($only)) {
+      $query->select(explode(',', $only));
+    }
 
-        $published = $this->request->getQuery('published');
-        if (!empty($published)) {
-            $query->where(['published' => 1]);
-        }
+    $published = $this->request->getQuery('published');
+    if (!empty($published)) {
+      $query->where(['published' => 1]);
+    }
 
-        // $limit = $this->request->getQuery('limit');
-        // if (!empty($limit)) {
-        //     $query->limit($limit);
-        // }
+    // $limit = $this->request->getQuery('limit');
+    // if (!empty($limit)) {
+    //     $query->limit($limit);
+    // }
 
-        $count = $this->request->getQuery('count');
-        if (!empty($count)) {
-            $count = $query->count();
-            $this->set('count', $count);
-            $this->viewBuilder()->setOption('serialize', 'count');
-        } else {
-            $destinations = $this->paginate($query, ['conditions' => ['show_in_list' => TRUE]]);
+    $count = $this->request->getQuery('count');
+    if (!empty($count)) {
+      $count = $query->count();
+      $this->set('count', $count);
+      $this->viewBuilder()->setOption('serialize', 'count');
+    } else {
+      $destinations = $this->paginate($query, ['conditions' => ['show_in_list' => TRUE]]);
 
-            $this->set(compact('destinations'));
-            $this->viewBuilder()->setOption('serialize', 'destinations');
-        }
+      $this->set(compact('destinations'));
+      $this->viewBuilder()->setOption('serialize', 'destinations');
+    }
   }
 
   /**
@@ -76,38 +79,38 @@ class DestinationsController extends AppController
     $query = $this->Destinations->find();
     $a = $this->request->getQuery('archive');
 
-		$articles_q = $this->Destinations->Articles->find()
-		->order(['modified' => 'DESC']);
+    $articles_q = $this->Destinations->Articles->find()
+      ->order(['modified' => 'DESC']);
 
-		if (empty($a)) {
-			$articles_q->where(['Articles.archived' => false]);
-		} else {
-			$articles_q->where(['Articles.archived' => true]);
-		}
+    if (empty($a)) {
+      $articles_q->where(['Articles.archived' => false]);
+    } else {
+      $articles_q->where(['Articles.archived' => true]);
+    }
 
-		if (is_string($id)) {
-			try {
-				$slug = $id;
-				$id = $this->Destinations->findBySlug($id)->firstOrFail()->id;
-			} catch (\Cake\Datasource\Exception\RecordNotFoundException $ex) {
-				$this->log(sprintf('Record not found in database (id = %d)!', $id), LogLevel::WARNING);
-			}
-		}
+    if (is_string($id)) {
+      try {
+        $slug = $id;
+        $id = $this->Destinations->findBySlug($id)->firstOrFail()->id;
+      } catch (\Cake\Datasource\Exception\RecordNotFoundException $ex) {
+        $this->log(sprintf('Record not found in database (id = %d)!', $id), LogLevel::WARNING);
+      }
+    }
 
-		$query->where(['id' => $id]);                   //Filtro le destination
-		$destination = $query->first();
-		$this->set('destination', $destination);
+    $query->where(['id' => $id]);                   //Filtro le destination
+    $destination = $query->first();
+    $this->set('destination', $destination);
 
-		$articles_q->where(['destination_id' => $id]);  //Filtro gli articoli
-		$articles_q->where(['published' => true]);      //Mostro solo quelli pubblicati
-		$art = $this->paginate($articles_q);
-		$this->set('articles', $art);
-		$this->set('archived', $a);
-		try {
-			$this->render($slug);
-		} catch (MissingTemplateException $e) {
-			$this->viewBuilder()->setTemplate(null);
-			$this->render(null);
-		}
-	}
+    $articles_q->where(['destination_id' => $id]);  //Filtro gli articoli
+    $articles_q->where(['published' => true]);      //Mostro solo quelli pubblicati
+    $art = $this->paginate($articles_q);
+    $this->set('articles', $art);
+    $this->set('archived', $a);
+    try {
+      $this->render($slug);
+    } catch (MissingTemplateException $e) {
+      $this->viewBuilder()->setTemplate(null);
+      $this->render(null);
+    }
+  }
 }
