cff5d52620f2ad54db306fd50a7fd5161f69eb58
Massimo Infunti <massimoi@nkoni.org>
2021-04-21 19:25:29 +0200

    correzioni v-autoload

--- templates/element/v-autoload.php
+++ templates/element/v-autoload.php
@@ -6,12 +6,12 @@ if (empty($v)) {
   $vue_name = "vue/$v";
 }
 
-$vue_path = $this->Url->script($vue_name);
+$vue_path = ltrim($this->Url->script($vue_name), '/');
 
 if (!empty($this->plugin) && file_exists(ROOT . "/plugins/{$this->plugin}/webroot{$vue_path}")) {
   echo $this->Html->script("{$this->plugin}.$vue_name");
 } else if (file_exists(WWW_ROOT . $vue_path)) {
-  echo $this->Html->script($vue_path);
+  echo $this->Html->script($vue_name);
 } else {
   echo $this->Html->script($this->Url->script("vue/app.js"));
 }
