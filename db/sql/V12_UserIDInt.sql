b6e762327467191fe329a79310c0cdcadc37fdbc
Massimo Infunti <massimoi@nkoni.org>
2021-04-22 14:30:11 +0200

    login compatibile con b3

--- db/sql/V12_UserIDInt.sql
+++ db/sql/V12_UserIDInt.sql
@@ -1,9 +1,11 @@
 ALTER TABLE `articles`
-CHANGE `user_id` `old_user_id` char(36) COLLATE 'utf8mb4_0900_ai_ci' NOT NULL AFTER `archived`,
+CHANGE `user_id` `old_user_id` char(36) NOT NULL AFTER `archived`,
 ADD `user_id` int NOT NULL AFTER `old_user_id`;
 
 ALTER TABLE `users`
-CHANGE `id` `oldid` char(36) COLLATE 'latin1_swedish_ci' NOT NULL FIRST,
-ADD `id` int NOT NULL AUTO_INCREMENT UNIQUE AFTER `oldid`;
+CHANGE `id` `oldid` char(36) NOT NULL FIRST,
+ADD `id` int NOT NULL AUTO_INCREMENT UNIQUE AFTER `oldid`,
+ADD `gmail` varchar(255) null,
+ADD `fbmail` varchar(255) null;
 
 update articles set user_id = 3;
