a654cec50a790f98f7941e7c0d074459cfbe75c5
alemi <ale.migliardi@gmail.com>
2021-05-26 17:51:21 +0200

    Aggiunti campi virtuali (url, copertina) e traduzione

--- src/Model/Entity/Destination.php
+++ src/Model/Entity/Destination.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Core\Configure;
+use Cake\I18n\I18n;
+use Cake\Utility\Text;
+use Cake\Filesystem\Folder;
+use Cake\Routing\Router;
 
 /**
  * Destination Entity
@@ -29,4 +34,42 @@ class Destination extends Entity
 	protected $_accessible = [
 		'*' => true,
 	];
+
+	protected $_virtual = ['copertina', 'url'];
+
+	public function _getUrl() {
+		$locale = I18n::getLocale();
+		$lang = Configure::read('LocaleMappings')[Configure::read('App.defaultLocale')];
+		if ($locale === $lang) {
+			return "/{$this->slug}";
+		}
+		return "/{$locale}/{$this->slug}";
+	}
+
+	public function _getCopertina() {
+		$fullDirTemplate = ':sitedir/:model/';
+		$fullDir = Text::insert($fullDirTemplate, [
+			'sitedir' => Configure::read('sitedir'),
+			'model' => 'destinations',
+		]);
+
+		// TODO: do this in a nicer way!
+		$fullDir = str_replace("//", "/", $fullDir);
+		$fullDir = str_replace("cyclomap.", "", $fullDir);
+
+		$dir = new Folder(WWW_ROOT . $fullDir);
+		$files = $dir->find("{$this->slug}\.(jpg|jpeg|gif|png|webp)", true);
+
+		if (is_array($files)) {
+			$files = $files[0];
+		}
+
+		/*Controllo se è vuoto*/
+		if (!$files) {
+			return Router::url(Configure::read('default-image', null));
+		}
+
+		$asd = preg_filter('/^/', Router::Url(str_replace(' ', '%20', $fullDir)), $files);
+		return $asd;
+	}
 }
