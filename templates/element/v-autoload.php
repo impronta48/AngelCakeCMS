86f89aad18de16a9881c1578351f794971f65981
alemi <ale.migliardi@gmail.com>
2021-05-31 17:59:09 +0200

    Fix per v-autoload! Non inserire in blocco

--- templates/element/v-autoload.php
+++ templates/element/v-autoload.php
@@ -10,10 +10,10 @@ if ($v === "mix") {
 
 $vue_path = ltrim($this->Url->script($vue_name), '/');
 
-if (!empty($this->plugin) && file_exists(ROOT . "/plugins/{$this->plugin}/webroot{$vue_path}")) {
-  echo $this->Html->script("{$this->plugin}.$vue_name", ['block' => true]);
+if (!empty($this->plugin) && file_exists(ROOT . "/plugins/{$this->plugin}/webroot/{$vue_path}")) {
+  echo $this->Html->script("{$this->plugin}.$vue_name");
 } else if (file_exists(WWW_ROOT . $vue_path)) {
-  echo $this->Html->script($vue_name, ['block' => true]);
+  echo $this->Html->script($vue_name);
 } else {
-  echo $this->Html->script($this->Url->script("vue/app.js", ['block' => true]));
+  echo $this->Html->script($this->Url->script("vue/app.js"));
 }
