cd28a5fe925e351dc101c6aa754e46778584eb28
alemi <me@alemi.dev>
2021-07-12 12:50:44 +0200

    Importa css bootstrap vue in v-autoload-import

--- templates/element/v-autoload-import.php
+++ templates/element/v-autoload-import.php
@@ -7,4 +7,5 @@ if ($v === "mix") {
 echo $this->Html->script("node_modules/vue/dist/vue.js");
 echo $this->Html->script("node_modules/bootstrap-vue/dist/bootstrap-vue.js");
 echo $this->Html->script("node_modules/bootstrap-vue/dist/bootstrap-vue-icons.js");
+echo $this->Html->css("/js/node_modules/bootstrap-vue/dist/bootstrap-vue.css");
 echo $this->Html->script('node_modules/axios/dist/axios.min');
