60957e8220117530eb5301d57ece961c6c5c86c2
alemi <me@alemi.dev>
2022-02-15 17:44:00 +0100

    Added language flags to change localization

--- templates/element/v-admin-header.php
+++ templates/element/v-admin-header.php
@@ -16,28 +16,39 @@ $this->element('v-admin-extra-main-menu'); ?>
       <b-navbar-nav>
         <b-nav-item href="<?= Router::url(['plugin' => false, 'prefix' => 'Admin', 'controller' => 'Articles', 'action' => 'index']) ?>">
           <b-icon-journal-text></b-icon-journal-text>
-          Articoli
+          <?= __('Articoli') ?>
         </b-nav-item>
         <?= $this->fetch('extra-main-menu') ?>
         <b-nav-item href="/admin/static/index">
           <b-icon-markdown></b-icon-markdown>
-          File Statici
+          <?= __('File Statici') ?>
         </b-nav-item>
         <b-nav-item href="/admin/blocks/index">
           <b-icon-bounding-box></b-icon-bounding-box>
-          Blocchi
+          <?= __('Blocchi') ?>
         </b-nav-item>
         <b-nav-item href="/admin/users">
           <b-icon-people></b-icon-people>
-          Utenti
+          <?= __('Utenti') ?>
         </b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+        <?php
+          $arr = $this->request->getAttribute('params');
+          unset($arr['lang']);
+        ?>
+        <?php foreach (Configure::read('I18n.languages') as $lang) : ?>
+          <?php $route = $lang != Configure::read('App.language') ? array_merge(['lang' => $lang], $arr) : $arr ; ?>
+          <b-nav-item href="<?= Router::reverse($route) ?>">
+            <?php $path = substr($lang, 0, 2) ?>
+            <?= $this->Html->image("flags/{$path}.png") ?>
+          </b-nav-item>
+        <?php endforeach; ?>
         <b-nav-item href="/" target="preview">
 
-          Anteprima sito
+          <?= __('Anteprima sito') ?>
           <b-icon-globe></b-icon-globe>
         </b-nav-item>
       </b-navbar-nav>
