b3d910f8334fae8e841839bd9e4a3ebbc7b12c43
MassimoI <massimoi@nkoni.org>
2022-03-23 20:04:56 +0100

    correzione route prezzi

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -32,9 +32,6 @@ require __DIR__ . '/paths.php';
  */
 require CORE_PATH . 'config' . DS . 'bootstrap.php';
 
-use Cake\Cache\Cache;
-use Cake\Cache\Engine\FileEngine;
-use Cake\Cache\Engine\MemcachedEngine;
 use Cake\Core\Configure;
 use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Database\TypeFactory;
@@ -95,69 +92,6 @@ try {
   exit($e->getMessage() . "\n");
 }
 
-// Cache::setConfig(Configure::consume('Cache'));
-/*
- * Configure generic caches.
- */
-Cache::setConfig('default', [
-  'className' => FileEngine::class,
-  'path' => CACHE . Configure::read('sitedir') . DS,
-]);
-Cache::setConfig('static', [
-  'className' => FileEngine::class,
-  'path' => CACHE . Configure::read('sitedir') . DS . 'static' . DS,
-]);
-/*
- * Configure the cache used for general framework caching.
- * Translation cache files are stored with this configuration.
- * Duration will be set to '+2 minutes' in bootstrap.php when debug = true
- * If you set 'className' => 'Null' core cache will be disabled.
- */
-Cache::setConfig('_cake_core_', [
-  'className' => FileEngine::class,
-  'prefix' => 'angelcake_core_',
-  'path' =>  CACHE . Configure::read('sitedir') . DS . 'persistent' . DS,
-  'serialize' => true,
-  'duration' => '+1 years',
-]);
-/*
- * Configure the cache for model and datasource caches. This cache
- * configuration is used to store schema descriptions, and table listings
- * in connections.
- * Duration will be set to '+2 minutes' in bootstrap.php when debug = true
- */
-Cache::setConfig('_cake_model_', [
-  'className' => FileEngine::class,
-  'prefix' => 'angelcake_model_',
-  'path' =>  CACHE . Configure::read('sitedir') . DS . 'models' . DS,
-  'serialize' => true,
-  'duration' => '+1 years',
-]);
-/*
- * Configure the cache for routes. The cached routes collection is built the
- * first time the routes are processed through `config/routes.php`.
- * Duration will be set to '+2 seconds' in bootstrap.php when debug = true
- */
-Cache::setConfig('_cake_routes_', [
-  'className' => FileEngine::class,
-  //'className' => MemcachedEngine::class,
-  'prefix' =>  Configure::read('sitedir') . '_cake_routes_',
-  'path' => CACHE . Configure::read('sitedir') . DS . 'routes' . DS,
-  //'serialize' => 'php',
-  'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
-  'duration' => '+1 years',
-  //'url' => env('CACHE_CAKEROUTES_URL', null),
-]);
-/*
- * Configure images cache.
- */
-Cache::setConfig('img', [
-  'className' => 'File',
-  'prefix' => 'angelcake_img_',
-  'path' =>  CACHE . Configure::read('sitedir') . DS . 'gallery' . DS,
-  'serialize' => true,
-  'duration' => '+1 years',
-]);
 
 //Carico la configurazione specifica per il filesystem
 //il plugin di riferimento è https://github.com/josbeir/cakephp-filesystem
@@ -186,6 +120,8 @@ if (Configure::read('debug')) {
   Configure::write('Cache._cake_core_.duration', '+2 minutes');
   Configure::write('Cache._cake_routes_.duration', '+2 minutes');
 }
+
+require_once "cache.php";
 
 /*
  * Set the default server timezone. Using UTC makes time calculations / conversions easier.
