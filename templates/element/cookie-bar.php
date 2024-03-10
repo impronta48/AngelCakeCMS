cc018b25870282a8cd53b3a5661208243fe15fe4
MassimoI <massimoi@nkoni.org>
2024-03-10 18:50:02 +0100

    aggiunta google tag manager

---
+++ templates/element/cookie-bar.php
@@ -0,0 +1,5 @@
+<?php
+// Aspetto un parametro privacy_url dal chiamante 
+// $privacy_url = "https://ebike.bikesquare.eu/pages/informativa-privacy";
+?>  
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=grey&tracking=1&thirdparty=1&always=1&showPolicyLink=1&privacyPage=<?= $privacy_url ?>"></script>
