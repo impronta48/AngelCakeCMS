ecd076dfb35400e4088514d07da1f607f30b7fe0
MassimoI <massimoi@nkoni.org>
2024-03-12 08:41:46 +0100

    correzione minified vue

--- templates/element/v-autoload-import.php
+++ templates/element/v-autoload-import.php
@@ -1,8 +1,17 @@
 <?php
+use Cake\Core\Configure;
+
 $v = $this->fetch('vue');
 
-echo $this->Html->script("node_modules/vue/dist/vue.js");
-echo $this->Html->script("node_modules/bootstrap-vue/dist/bootstrap-vue.js");
+$d = Configure::read('debug');
+if ($d) {
+    echo $this->Html->script("node_modules/vue/dist/vue.js");
+    echo $this->Html->script("node_modules/bootstrap-vue/dist/bootstrap-vue.js");
+} else {
+    echo $this->Html->script("node_modules/vue/dist/vue.min.js");
+    echo $this->Html->script("node_modules/bootstrap-vue/dist/bootstrap-vue.min.js");
+}
+
 echo $this->Html->script('node_modules/bootstrap-vue/dist/bootstrap-vue-icons.min.js');
 echo $this->Html->script('node_modules/axios/dist/axios.min');
 
