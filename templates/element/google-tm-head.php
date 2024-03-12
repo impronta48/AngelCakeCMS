0f99e95940bedd0bca37512d477ced26979da148
MassimoI <massimoi@nkoni.org>
2024-03-12 10:22:58 +0100

    verifica cookies

--- templates/element/google-tm-head.php
+++ templates/element/google-tm-head.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?customize=1&tracking=1&thirdparty=1&always=1&privacyPage=<?= urlencode($privacy_url) ?>"></script>
 
 
-<?php if ($_COOKIE['cookiebar'] == "CookieAllowed") : ?>
+<?php if (isset($_COOKIE['cookiebar']) && $_COOKIE['cookiebar'] == "CookieAllowed") : ?>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $id ?>"></script>
 <script>
