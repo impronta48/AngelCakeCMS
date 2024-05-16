c7f10525ae9752010221a349314fd5950d720a35
Massimo Infunti <massimoi@nkoni.org>
2024-05-16 18:25:37 +0200

    Miglorie della struttura dell'articolo

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -4,19 +4,22 @@ use Cake\Routing\Router;
 
 ?>
 <div class="container">
-  <h4>Articoli</h4>
+  <h4>I nostri aggiornamenti</h4>
   <div class="card-deck">
 
     <?php foreach ($articles as $a) : ?>
       <?php $dname = (!empty($a->destination)) ? $a->destination->name : ''; ?>
       <div class="card" style="max-width: 18rem;">
+        <a href="<?= Router::url(['action' => 'view', $a->slug]) ?>">
         <img class="card-img-top" src="/images<?= $a->copertina ?>?w=309&h=200&fit=crop&fm=webp" alt="<?= $dname  . ", " . $a->title ?>">
+        </a>
         <div class="card-body">
-          <h5 class="card-title"><?= $a->title ?></h5>
+          <h5 class="card-title">
+            <a href="<?= Router::url(['action' => 'view', $a->slug]) ?>">
+              <?= $a->title ?>
+            </a>
+            </h5>
           <p class="card-text"><?= strip_tags($this->Text->truncate($a->body, 200)); ?></p>
-        </div>
-        <div class="card-footer">
-          <a href="<?= Router::url(['action' => 'view', $a->slug]) ?>" class="btn btn-secondary btn-sm mt-2 float-right">Leggi <?= $a->title ?></a>
         </div>
       </div>
 
