9865570a14dc9ef6b56f82b3cbe455a7007d6d48
Matteo Somale <matteo.somale@gmail.com>
2025-02-06 12:54:11 +0100

    no more img borders

--- templates/element/img-gallery.php
+++ templates/element/img-gallery.php
@@ -2,10 +2,10 @@
 
 <div class="row">
   <div class="col">
-    <?php foreach ($images as $img) : ?>
+    <?php foreach ($images as $img): ?>
 
       <a class="spotlight" href="<?= "/images/$img?w=1042&fm=webp" ?>">
-        <img src="<?= "/images/$img?h=150&w=150&fit=crop&fm=webp" ?>" class="img-thumbnail img-fluid">
+        <img src="<?= "/images/$img?h=150&w=150&fit=crop&fm=webp" ?>" class="img-fluid rounded p-1">
       </a>
 
     <?php endforeach ?>
