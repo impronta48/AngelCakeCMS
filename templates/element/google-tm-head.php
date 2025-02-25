4c3dfb5bfb4729c20adca49374b7d24fd0020610
Massimo Infunti <massimoi@nkoni.org>
2025-02-25 08:04:43 +0100

    tolto il cookiebar

--- templates/element/google-tm-head.php
+++ templates/element/google-tm-head.php
@@ -4,11 +4,6 @@ use Cake\Core\Configure;
 
 if (Configure::read('debug') == true) return; ?>
 
-
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?customize=1&tracking=1&thirdparty=1&always=1&privacyPage=<?= urlencode($privacy_url) ?>"></script>
-
-
-
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $id ?>"></script>
 <script>
