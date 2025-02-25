ea294736f20f80d0ffb61e9b08d6772ac7331ba1
Massimo Infunti <massimoi@nkoni.org>
2025-02-25 08:25:01 +0100

    aggiornamento tag manager

---
+++ templates/element/google-tm-body.php
@@ -0,0 +1,10 @@
+<?php
+
+use Cake\Core\Configure;
+
+if (Configure::read('debug') == true) return; ?>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $id ?>"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
