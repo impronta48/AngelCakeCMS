bf2cfbd1a056a247df8c522a25d99be92cc6831c
Massimo Infunti <massimoi@nkoni.org>
2025-02-25 08:27:48 +0100

    correzione virgolette mancanti

--- templates/element/google-tm-head.php
+++ templates/element/google-tm-head.php
@@ -9,5 +9,5 @@ if (Configure::read('debug') == true) return; ?>
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',<?= $id ?>);</script>
+})(window,document,'script','dataLayer','<?= $id ?>');</script>
 <!-- End Google Tag Manager -->
