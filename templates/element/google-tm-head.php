cd73b83453ebf54a39a97951f22aa7a54c3bada9
Massimo Infunti <massimoi@nkoni.org>
2025-02-25 07:55:30 +0100

    tolto cookie banner

--- templates/element/google-tm-head.php
+++ templates/element/google-tm-head.php
@@ -8,7 +8,7 @@ if (Configure::read('debug') == true) return; ?>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?customize=1&tracking=1&thirdparty=1&always=1&privacyPage=<?= urlencode($privacy_url) ?>"></script>
 
 
-<?php if (isset($_COOKIE['cookiebar']) && $_COOKIE['cookiebar'] == "CookieAllowed") : ?>
+
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $id ?>"></script>
 <script>
@@ -22,4 +22,3 @@ if (Configure::read('debug') == true) return; ?>
     gtag('config', "<?= $id ?>");
 </script>
 <!-- End Google Tag Manager -->
-<?php endif; ?>
