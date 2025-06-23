9b0231fb1501ddcbe3607566828715390a8e0d0b
Massimo Infunti <massimoi@nkoni.org>
2025-06-23 23:36:53 +0200

    rotazione zero delle immagini

--- templates/element/img-gallery.php
+++ templates/element/img-gallery.php
@@ -4,8 +4,8 @@
   <div class="col">
     <?php foreach ($images as $img): ?>
 
-      <a class="spotlight" href="<?= "/images/$img?w=1042&fm=webp" ?>">
-        <img src="<?= "/images/$img?h=150&w=150&fit=crop&fm=webp" ?>" class="img-fluid rounded p-1">
+      <a class="spotlight" href="<?= "/images/$img?w=1042&fm=webp&or=0" ?>">
+        <img src="<?= "/images/$img?h=150&w=150&fit=crop&fm=webp&or=0" ?>" class="img-fluid rounded p-1">
       </a>
 
     <?php endforeach ?>
